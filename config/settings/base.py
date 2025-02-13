c8ae6473004d9ddf1382bb892dbf0b995d5c81c3
Miguel-Pinto99 <miguelpinto1999@gmail.com>
2025-02-13 12:33:06 +0000

    Change REST_FRAMEWORK permissions

--- config/settings/base.py
+++ config/settings/base.py
@@ -333,7 +333,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES": (
-        "rest_framework.permissions.IsAuthenticated",  # Require authentication by default
+        "rest_framework.permissions.AllowAny",  # Require authentication by default
     ),
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema", 
 }
