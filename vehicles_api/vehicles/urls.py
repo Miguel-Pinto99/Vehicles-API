ad2311666b7c10c6ee645f0c1b19d728339c1248
Miguel-Pinto99 <miguelpinto1999@gmail.com>
2025-02-14 10:52:41 +0000

    Add default and blank values to models

--- vehicles_api/vehicles/urls.py
+++ vehicles_api/vehicles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import VehicleDetailAPIView, VehicleListAPIView
+
 urlpatterns = [
     path('Vehicles/', VehicleListAPIView.as_view(), name='Vehicle-list-create'),
-    path('Vehicles/<str:plate>/', VehicleDetailAPIView.as_view(), name='Vehicle-detail'),
+    path('Vehicles/<int:pk>/', VehicleDetailAPIView.as_view(), name='Vehicle-detail'),
 ]
